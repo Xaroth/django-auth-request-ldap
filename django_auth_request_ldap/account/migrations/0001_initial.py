2775ea1bb3250ab5926387c0adb36324447e780f
Xaroth Brook <xaroth@xaroth.nl>
2015-06-18 17:31:16 +0200

    -Add: Initial migrations to make other mock database tables.

--- django_auth_request_ldap/account/migrations/0001_initial.py
+++ django_auth_request_ldap/account/migrations/0001_initial.py
@@ -15,5 +15,34 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
             ],
+            options={
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
+                'managed': False,
+            }
+        ),
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+                'verbose_name': 'group',
+                'verbose_name_plural': 'groups',
+                'managed': False,
+            }
+        ),
+        migrations.CreateModel(
+            name='SambaDomainName',
+            fields=[
+                ('domain_name', models.CharField(verbose_name='ID', max_length=200,
+                                                 serialize=False, primary_key=True,
+                                                 db_column=b'sambaDomainName')),
+            ],
+            options={
+                'verbose_name': 'samba domain name',
+                'verbose_name_plural': 'samba domain names',
+                'managed': False,
+            }
         ),
     ]
